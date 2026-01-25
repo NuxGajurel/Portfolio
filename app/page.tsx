3d9f6b8104ae7f58289cb9ffb38ca75e1aeb10e4
Nux Gajurel <gajurelnux30@gmail.com>
2026-01-25 17:14:29 +0545

    link add

--- app/page.tsx
+++ app/page.tsx
@@ -178,7 +178,28 @@ export default function Home() {
      </div>
   
         </div>
-        
+        <div>
+         
+<button className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
+border-blue-600
+border-b-4px hover:brightness-110 hover:-translate-y-1px hover:border-b-[6px]
+active:border-b-2px active:brightness-90 active:translate-y-2px w-full mt-5">
+ See All 
+</button>
+
+        </div>
+      <div className="mt-1 text-right">
+  <p className="text-xs text-gray-400">
+    Photos page is inspired by{" "}
+    <a
+      href="https://manishtamang.com/photos"
+      className="text-blue-400 hover:underline"
+    >
+      golecodes
+    </a>
+  </p>
+</div>
+
       </div>
     </main>
   );
