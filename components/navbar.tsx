e600f38dc3209e9c16ebc2312f43552028e14b11
Nux Gajurel <gajurelnux30@gmail.com>
2026-02-15 10:23:27 +0545

    la

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,6 @@ import { CiMenuBurger } from "react-icons/ci";
 import { RxCross2 } from "react-icons/rx";
 import { IoBulbOutline } from "react-icons/io5";
 
-
 type NavItem = {
   name: string;
   path: string;
@@ -36,12 +35,7 @@ const Navbar = () => {
             </Link>
           ))}
         </nav>
-<div className="flex flex-col items-center gap-0">
-  <div className="w-8 h-9 flex items-center justify-center rounded-full bg-white transition-all">
-    <IoBulbOutline size={22} className="text-gray-700" />
-  </div>
-  
-</div>
+        
         {/* Mobile Menu Button */}
         <i
           className="md:hidden"
@@ -50,9 +44,13 @@ const Navbar = () => {
         >
           {open ? <RxCross2 size={20} /> : <CiMenuBurger size={20} />}
         </i>
+        <div className="flex flex-col items-center gap-0">
+          <div className="w-8 h-9 flex items-center justify-center rounded-full bg-white transition-all">
+            <IoBulbOutline size={22} className="text-gray-700" />
+          </div>
+        </div>
       </div>
-  
-      
+
       <hr className="mt-4 m-0 p-0 text-gray-300"></hr>
       {/* Mobile Menu */}
       {open && (
@@ -65,9 +63,12 @@ const Navbar = () => {
             >
               {item.name}
             </Link>
+            
           ))}
         </nav>
+        
       )}
+      
     </header>
   );
 };
