6c0f9030351290ca55c38706432cff7a88095389
Nux Gajurel <gajurelnux30@gmail.com>
2026-04-12 14:10:24 +0545

    half-About page finished

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,76 @@
 import React from 'react'
+import {Dancing_Script} from "next/font/google";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { SiDailydotdev } from "react-icons/si";
+
+const dancingScript = Dancing_Script({
+  subsets: ["latin"],
+  weight: "400",
+})
 
 const page = () => {
+  const links =[
+    {name: "GitHub", path:"https://github.com/NuxGajurel", icons:<FaGithub/>},
+    {name: "Instagram", path:"https://www.instagram.com/nuxgajurel/", icons:<FaInstagram/>},
+    {name: "LInkedin", path:"https://www.linkedin.com/in/nux-gajurel-355962348/ ,", icons:<FaLinkedin/>},
+    {name:"Daily.dev", path:"https://app.daily.dev/nuxgajurel", icons:<SiDailydotdev/>},
+  ]
+  const Photo ={
+    name: "Nux Gajurel",
+    avatarUrl: "/end.jpg",
+  }
+  const school = {
+  name: "The Rising English Boarding School",
+  logoUrl: "/school.png",
+};
   return (
     <div> 
+   <div className=''>
+   <div>
+     <h1 className="text-2xl" >About </h1>
+     <p className={`${dancingScript.className} text-2xl mt-3`}>Hi, I'm Nawaraj Gajurel (aka Nux), a passionate aspiring Full-Stack Developer from Nepal. This is the space where I showcase my work and the things I love to create.</p>
+   </div>
+<div className="flex items-center gap-4 mt-6">
+  {links.map((item) => (
+    <i key={item.name} className={`text-xl cursor-pointer hover:text-blue-500 hover:text-{item.name}`} aria-setsize={28}>
+      {item.icons}
+    </i>
+  ))}
+</div>
+   </div>
+   {/*  */}
+  <div className="mt-10 w-full">
+  <img
+    src={Photo.avatarUrl}
+    alt={Photo.name}
+    className="w-full h-64 object-cover rounded-lg"
+  />
+</div>
+<p className='flex justify-end mt-3 text-gray-400 hover:text-black'> - Last Day of SEE (4/9/2026 Thursday)</p>
+   
+   <div>
+    <h1 className='text-2xl mt-7'>Education </h1>
+    <div className='mt-8'>
+<div className="flex justify-baseline gap-0">
+            <div className="rounded h-24 w-24 hover:scale-105 ">
+              <img src={school.logoUrl} alt="ksks" className="object-cover" />
+            </div>
+            <div className=''>
+              <h1 className="hover:text-blue-500 lg:text-2xl">{school.name}</h1>
+              <p className="items-center text-gray-400">2013-2026</p>
+              <p>High School (10th Grade)</p>
+            </div>
+            <div className="hidden md:block">
+              <a href="https://www.facebook.com/p/The-Rising-English-Secondary-Boarding-school-Biratchowk-100063526275005/">
+              
+              </a>
+            </div>
+          </div>
+        </div>
+    </div>
+   
+   
+   
    
     </div>
   )
