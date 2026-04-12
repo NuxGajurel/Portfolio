5a603566818262b5007044a145100f25555d1609
Nux Gajurel <gajurelnux30@gmail.com>
2026-04-12 14:22:19 +0545

    clean code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,72 +1,62 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
 
 import { FiTool, FiFolder, FiCamera, FiUser } from "react-icons/fi";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { SiDailydotdev } from "react-icons/si";
 
-
-const footer = () => {
+const Footer = () => {
   const nav = [
-  { name:"About", path: "/about" ,icons:<FiUser />
-},
-    { name: "Uses", path: "/uses" ,icons:<FiTool/>},
-    { name: "Projects", path: "/projects", icons:<FiFolder/> },
-    { name: "Photos", path: "/photos" ,icons:<FiCamera/> },
-   
+    { name: "About", path: "/about", icons: <FiUser /> },
+    { name: "Uses", path: "/uses", icons: <FiTool /> },
+    { name: "Projects", path: "/projects", icons: <FiFolder /> },
+    { name: "Photos", path: "/photos", icons: <FiCamera /> },
   ];
-  const [open, setOpen] = useState(false);
- const socialLinks = [
-  {name: "GitHub", path:"https://github.com/NuxGajurel ", icons:<FaGithub/>},
-   {name: "Instagram", path:"https://www.instagram.com/nuxgajurel/", icons:<FaInstagram/>},
-   {name: "LInkedin", path:"https://www.linkedin.com/in/nux-gajurel-355962348/ ", icons:<FaLinkedin/>},
-   {name:"Daily.dev", path:"https://app.daily.dev/nuxgajurel", icons:<SiDailydotdev/>},
-  
- ];
-  return (
-    <div>
-      <footer className="min-h-screen px-0 sm:px-0 lg:px-6 lg:py-16 py-10">
-        <div className="flex justify-baseline lg:gap-15 gap-5">
-          <div className="flex flex-col col-end-4 space-y-1">
-            {nav.map((item) => (
-              <div key={item.name}>
-                <Link
-                  key={item.path}
-                  href={item.path}
-                  onClick={() => setOpen(false)}
-                >
-                 <p className="flex items-center gap-2 text-black">
-  {item.icons}
-  {item.name}
-</p>
 
-                </Link>
-              </div>
-            ))}
-          </div>
-          <div>
-             <div className="flex flex-col col-end-4 space-y-1">
-            {socialLinks.map((social) => (
-              <div key={social.name}>
-                <a
-                  key={social.path}
-                  href={social.path}
-                  onClick={() => setOpen(false)}
-                >
-                    <p className="flex items-center gap-2 text-black">
-  {social.icons}
-  {social.name}
-</p>
-                </a>
-          </div>
-        ))}
-          </div>
-          </div>
+  const socialLinks = [
+    { name: "GitHub", path: "https://github.com/NuxGajurel", icons: <FaGithub /> },
+    { name: "Instagram", path: "https://www.instagram.com/nuxgajurel/", icons: <FaInstagram /> },
+    { name: "LinkedIn", path: "https://www.linkedin.com/in/nux-gajurel-355962348/", icons: <FaLinkedin /> },
+    { name: "Daily.dev", path: "https://app.daily.dev/nuxgajurel", icons: <SiDailydotdev /> },
+  ];
+
+  return (
+    <footer className="px-4 lg:px-6 py-10 lg:py-16">
+      <div className="flex gap-10 lg:gap-16 items-start">
+        
+        {/* Navigation */}
+        <div className="flex flex-col space-y-2">
+          {nav.map((item) => (
+            <Link key={item.name} href={item.path}>
+              <p className="flex items-center gap-2 text-black hover:text-blue-500 cursor-pointer">
+                {item.icons}
+                {item.name}
+              </p>
+            </Link>
+          ))}
         </div>
-      </footer>
-    </div>
+
+        {/* Social Links */}
+        <div className="flex flex-col space-y-2">
+          {socialLinks.map((social) => (
+            <a
+              key={social.name}
+              href={social.path}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="flex items-center gap-2 text-black hover:text-blue-500 cursor-pointer">
+                {social.icons}
+                {social.name}
+              </p>
+            </a>
+          ))}
+        </div>
+
+      </div>
+    </footer>
   );
 };
 
-export default footer;
+export default Footer;
